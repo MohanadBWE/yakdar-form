d2165c807fb43793ca84588f9efefa3e9e2a7d6d
Mohanad Mala <113777288+MohanadBWE@users.noreply.github.com>
2026-02-04 10:45:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ CATEGORIZED_FORMS = {
     
     },
     "Sumel - Mserik": {
-        "mserik 26.1.26": "https://ee.kobotoolbox.org/x/Qcu0BQT5",
+        "mserik 04.02.2026": "https://ee.kobotoolbox.org/x/bOSjuqWb",
 
     },
     "forms": {
@@ -235,6 +235,7 @@ st.info(
 
 # Close the main container div
 st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
